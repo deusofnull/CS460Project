ccfdb8260aaa6a850752036a5d91bf27f5f6393b
unknown <wacheson@CSLAB-12.cs.hartford.edu>
2013-04-11 16:59:20 -0400

    Added user database methods into the database classs, also added user class

--- src/videorentalstore/VideoRentalStore.java
+++ src/videorentalstore/VideoRentalStore.java
@@ -1,7 +1,7 @@
 package videorentalstore;
 
 import videorentalstore.database.Database;
-
+import videorentalstore.User.User;
 
 /**
  *
@@ -14,15 +14,35 @@ public class VideoRentalStore {
      * @throws Exception 
      */
     public static void main(String[] args) throws Exception {
+        
+        
+        
         Database rentalStore = new Database("movies.db");
         System.out.println("Database loaded\n");
         //rentalStore.initDatabaseTables();
         //System.out.println("Tables created");
+        
         rentalStore.findMoviesByTitle("Star Wars");
         System.out.print("\n");
         rentalStore.findMoviesByTitle("of the");
         System.out.print("\n");
         rentalStore.findMoviesByDirector("ge Lu");
         System.out.println("\nDone running");
+        
+        User u = new User(1,"Will","Acheson","willyach07","password");
+        
+        //rentalStore.addUsertoDB(u);
+        
+        //rentalStore.searchUserinDBbyID(50);
+        rentalStore.searchUserinDBbyID(1);
+        
+        rentalStore.searchUserinDBbyLastName("Acheson");
+        
+        rentalStore.displayTable();
+        //rentalStore.deleteUser("Will", "Acheson");
+        
+        
+        
+        
     }
 }
--- src/videorentalstore/database/Database.java
+++ src/videorentalstore/database/Database.java
@@ -3,8 +3,12 @@ package videorentalstore.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.Collection;
+
+
+import videorentalstore.User.User;
 
 import videorentalstore.movies.Movie;
 
@@ -15,6 +19,7 @@ public class Database {
     private String Jdbc = "jdbc:sqlite";
     private int timeout = 30;
     private Collection<Movie> movies;
+    private Collection<User> users;
     
     public Database(String DbName) throws Exception {
         // register the driver 
@@ -30,9 +35,13 @@ public class Database {
     }
     
     public void initDatabaseTables() throws Exception {
-        executeUpdate("CREATE TABLE \"customer\" (\"customerId\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE , \"firstName\" VARCHAR NOT NULL , \"lastName\" VARCHAR NOT NULL , \"email\" VARCHAR NOT NULL , \"password\" VARCHAR NOT NULL , \"birthday\" VARCHAR, \"creditCardNum\" INTEGER, \"creditCardExpDate\" VARCHAR, \"address\" VARCHAR, \"city\" VARCHAR, \"state\" VARCHAR, \"zipcode\" INTEGER)");
-        executeUpdate("CREATE TABLE \"movies\" (\"movieId\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE , \"title\" VARCHAR NOT NULL , \"genre\" VARCHAR NOT NULL , \"director\" VARCHAR NOT NULL , \"MPAA\" VARCHAR NOT NULL , \"userRating\" INTEGER NOT NULL )");
-        executeUpdate("CREATE TABLE \"rentals\" (\"rentalID\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE ,\"customerID\" INTEGER NOT NULL ,\"movieID\" INTEGER NOT NULL ,\"timeRented\" DATETIME NOT NULL DEFAULT (CURRENT_DATE) ,\"timeDue\" DATETIME NOT NULL DEFAULT (CURRENT_DATE) )");
+        executeUpdate("CREATE TABLE \"customer\" (\"customerId\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE , \"firstName\" VARCHAR NOT NULL ,"
+                + " \"lastName\" VARCHAR NOT NULL , \"email\" VARCHAR NOT NULL , \"password\" VARCHAR NOT NULL , \"birthday\" VARCHAR, \"creditCardNum\" INTEGER, "
+                + "\"creditCardExpDate\" VARCHAR, \"address\" VARCHAR, \"city\" VARCHAR, \"state\" VARCHAR, \"zipcode\" INTEGER)");
+        executeUpdate("CREATE TABLE \"movies\" (\"movieId\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE , \"title\" VARCHAR NOT NULL , "
+                + "\"genre\" VARCHAR NOT NULL , \"director\" VARCHAR NOT NULL , \"MPAA\" VARCHAR NOT NULL , \"userRating\" INTEGER NOT NULL )");
+        executeUpdate("CREATE TABLE \"rentals\" (\"rentalID\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE ,\"customerID\" INTEGER NOT NULL , "
+                + "\"movieID\" INTEGER NOT NULL ,\"timeRented\" DATETIME NOT NULL DEFAULT (CURRENT_DATE) ,\"timeDue\" DATETIME NOT NULL DEFAULT (CURRENT_DATE) )");
     }
     
     private void createMovieObjects() throws Exception {
@@ -103,6 +112,7 @@ public class Database {
             while (directors.next()) {
                 String dirName = directors.getString("director");
                 System.out.println(dirName);
+                System.out.println(search);
                 //TODO make this do something else, possibly add to a collection of Movie Objects
             }
             try {
@@ -121,8 +131,120 @@ public class Database {
      *        Customer Table Stuff         *
      *                                     *
      ***************************************/
+    public void addUsertoDB(User user){
+        //Statement stmt;
+        boolean userAddRES;
+        try {
+            String insert = "INSERT INTO customer (customerID , lastName , firstName , email, password) VALUES ( " +  user.getCustomerID() + " , " + " ' " + user.getLastName() + " ' " +
+                " , " + " ' " + user.getFirstName() + " ' " +  " , " + " ' " +  user.getEmail() + " ' " + " , " + " ' " + user.getPassword() + " ' " + " ) ";
+            System.out.println(insert);
+            stmt = conn.createStatement();
+            userAddRES = stmt.execute(insert);
+        }
+        catch (Exception e){
+            System.out.println("Error adding user to Database: " + e.toString());
+            try {
+                conn.close();
+            }
+            catch (Exception e1){
+            }
+        }
+    }
     
+    public void searchUserinDBbyID(int ID){
+        ResultSet searchUserRES;
+        try {
+            String search = "SELECT * FROM customer WHERE customerID = " + ID ;
+            stmt = conn.createStatement();
+            searchUserRES = stmt.executeQuery(search);
+            while (searchUserRES.next()) {
+                
+                String user = "First Name: " + searchUserRES.getString("firstName") + "Last Name: " +searchUserRES.getString("lastName") + 
+                        "Email Address" + searchUserRES.getString("email") + "Password:  " + searchUserRES.getString("password");
+                System.out.println("User Found at searched ID " + ID + ": " + user);
+            }    
+        }
+        catch (Exception e){
+            
+            try {
+                
+                conn.close();
+            }
+            catch (Exception e1){
+            }
+            
+        }
+        
+    }
+   
+    public void searchUserinDBbyLastName(String lastName){
+        ResultSet searchUserLastNameRES;
+        try {
+            String search = "SELECT * FROM customer WHERE lastName = " + " ' " + lastName + " ' " ;
+            stmt = conn.createStatement();
+            searchUserLastNameRES = stmt.executeQuery(search);
+            while (searchUserLastNameRES.next()) {
+                
+                String user = "First Name: " + searchUserLastNameRES.getString("firstName") + "Last Name: " +searchUserLastNameRES.getString("lastName") + 
+                        "Email Address" + searchUserLastNameRES.getString("email") + "Password:  " + searchUserLastNameRES.getString("password");
+                System.out.println("User Found at Last Name: " + lastName + ": " + user);
+            }    
+        }
+        catch (Exception e){
+            
+            try {
+                
+                conn.close();
+            }
+            catch (Exception e1){
+            }
+            
+        }
+        
+    }
+   
+    //doesnt work yet
+    public void displayTable(){
+        ResultSet customerTable ; 
+        try {
+            String wholeTable = "SELECT * FROM customer";
+            stmt = conn.createStatement();
+            customerTable = stmt.executeQuery(wholeTable);
+            
+        }
+        
+        catch (Exception e){
+            
+            try {
+                
+                conn.close();
+            }
+            catch (Exception e1){
+            }
+            
+        }
+    }
+     
     
+     
+    public void deleteUser(String firstName , String lastName){
+        boolean userDeleteRES;
+        try {
+            String delete = "DELETE FROM customer WHERE lastName = " + " ' " + lastName + " ' " + "AND firstName = " + " ' " + firstName + " ' ";
+            System.out.println(delete);
+            stmt = conn.createStatement();
+            userDeleteRES = stmt.execute(delete);
+            System.out.println("User: " + firstName + lastName + " was deleted from the database.");
+        }
+        catch (Exception e){
+            System.out.println("Error deleting user from Database: " + e.toString());
+            try {
+                conn.close();
+            }
+            catch (Exception e1){
+            }
+        }
+    }
     
     /***************************************
      *                                     *
